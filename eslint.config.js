ec90565541e9f8c097d30dc42373b193051c0f6f
curious coder <kuriousdev@gmail.com>
2025-09-22 00:28:59 +0530

    feat: add eslint-plugin-web-perf for performance linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const webPerfPlugin = require('@curiousdev-oss/eslint-plugin-web-perf');
 
 module.exports = tseslint.config(
   {
@@ -11,6 +12,7 @@ module.exports = tseslint.config(
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      webPerfPlugin.configs.recommended,
     ],
     processor: angular.processInlineTemplates,
     rules: {
